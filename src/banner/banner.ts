92f9eaac9b47ee9b5e090e75404033bb11d9f1ca
Geoff Koros <korosmatick@gmail.com>
2024-02-14 13:45:15 +0200

    fix path issue

--- src/banner/banner.ts
+++ src/banner/banner.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 // Define the file path
-const filePath = 'src/banner/banner.txt';
+const filePath = './src/banner/banner.txt';
 
 export function printBanner(): void {
   try {
